00d12872adcf25b8d9f567087c13df7dbfcb0093
Boglarka Nagy <thebogosz@gmail.com>
2017-10-07 00:18:10 +0200

    resident button visible, registration and login bit desinged

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,50 @@
-//get the data
 $(function(){
     var $items = $('#items');
-    $.ajax({
-       type: 'GET',
-       url: 'https://swapi.co/api/planets/',
-       success: function(items) {
-           $.each(items.results, function(i, item){
-            $items.append('<tr><td>' + item.name + '</td>' +
-                          '<td>' + item.diameter + '</td>' +
-                          '<td>' + item.climate + '</td>' +
-                          '<td>' + item.terrain + '</td>' +
-                          '<td>' + item.surface_water + '</td>' +
-                          '<td>' + item.population + '</td>' +
-                          '<td>' + item.residents + '</td></tr>'
-                         );
-           })
-       }
-    });
-    var i = 1;
-        $('#next').on('click', function(){
-            i++;
-            listObj.show(i, 10); 
-        })
+    var $pagecount = 1;
+        //fill the table  
+        //handle no data, create button 
+        function residentHandler(resident) {
+        if (resident.length === 0) {
+            return 'No known resident'
+        } else {
+            return '<button type="button" class="btn btn-secondary residentbutton" data-residents="' + resident + '" >' + resident.length + ' resident(s)</button>'
+        }};
 
-        $('#prev').on('click', function(){
-            i--;
-            listObj.show(i, 10); 
-        })
-});
-//next-prev button
+        $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/planets/',
+        success: function(items) {
+            $.each(items.results, function(i, item){
+                $items.append('<tr><td>' + item.name + '</td>' +
+                            '<td>' + item.diameter + '</td>' +
+                            '<td>' + item.climate + '</td>' +
+                            '<td>' + item.terrain + '</td>' +
+                            '<td>' + item.surface_water + '</td>' +
+                            '<td>' + item.population + '</td>' +
+                            '<td>' + residentHandler(item.residents) + '</td></tr>'
+                            );
+           });
+        },
+        error: function(){
+                alert('error loading planets');
+        }
+        });
+    $("#next, #previous").click(function() {
+        $pagecount = ($(this).attr('id')=='next') ? $pagecount + 1 : $pagecount - 1;
+        if ($pagecount === 0) {
+            $pagecount = 1;
+        } else if ($pagecount === 8){
+            $pagecount = 7;
+        }
+    });
+    },
+        //handle no data, create button 
+        function residentHandler(resident) {
+        if (resident.length === 0) {
+            return 'No known resident'
+        } else {
+            return '<button type="button" class="btn btn-secondary residentbutton" data-residents="' + resident + '" >' + resident.length + ' resident(s)</button>'
+        }
+    });
+    
+    
